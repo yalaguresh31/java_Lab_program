80b3d6beb41fef322432b4249470ec033c6e8f4d
yalaguresh31 <123857002+yalaguresh31@users.noreply.github.com>
2023-04-05 14:11:16 +0530

    Update LP3b_ArithmeticCalculator.java

--- java_Lab/LP3b_ArithmeticCalculator.java
+++ java_Lab/LP3b_ArithmeticCalculator.java
@@ -5,13 +5,13 @@ public class LP3b_ArithmeticCalculator{
     public static void main(String[] args) {
         char op;
         double n1,n2;
-        try (Scanner s = new Scanner(System.in)) {
+        Scanner s = new Scanner(System.in)
           System.out.println("Enter the two numbers");
               n1 = s.nextDouble();
                n2 = s.nextDouble();
                System.out.println("Enten the op");
                op = s.next().charAt(0);
-        }
+        
         //inhens switch
             switch (op) {
             case '+'->  System.out.println("sum ="+(n1 + n2));
@@ -20,30 +20,6 @@ public class LP3b_ArithmeticCalculator{
             case '/'->  System.out.println("div ="+(n1/n2)); 
             default ->  System.out.println("your enter invalid input");     
          }
-
-    //    double result = switch (op) {
-    //       case '+'->  (n1 + n2);
-    //       case '-'->  (n1 - n2);
-    //       case '*'->  (n1 * n2);
-    //       case '/'->  (n1/n2); 
-    //       //default ->  System.out.println("your enter invalid input");
-    //     default -> throw new IllegalArgumentException("your enter invalid input " + op);
-    // };
-    // System.out.println(result);
-
-
-
-//     double result = switch (op) {
-//       case '+': yield  (n1 + n2);
-//       case '-': yield  (n1 - n2);
-//       case '*': yield  (n1 * n2);
-//       case '/': yield  (n1/n2); 
-//       //default : yield  System.out.println("your enter invalid input");
-//     default : throw new IllegalArgumentException("your enter invalid input " + op);
-// };
-// System.out.println(result);
-
-
-}
+   }
 }
 
